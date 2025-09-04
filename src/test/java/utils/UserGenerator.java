1b38e57186a0d5ebd5cee47b64b8bdb3fe1bae71
AlinaNefedowa <nefedowa.as@gmail.com>
2025-09-04 22:47:34 +0200

    created LoginTest

---
+++ src/test/java/utils/UserGenerator.java
@@ -0,0 +1,13 @@
+package utils;
+
+import models.User;
+
+import java.util.UUID;
+
+public class UserGenerator {
+
+    public static User getRandomUser() {
+        String uniqueEmail = "test_" + UUID.randomUUID() + "@yandex.ru";
+        return new User(uniqueEmail, "123456", "TestUser");
+    }
+}
